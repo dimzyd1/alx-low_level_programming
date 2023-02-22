9d5f64e3d9942ba2589a666cb26d5d7b138132ee
dimzyd1 <106175940+dimzyd1@users.noreply.github.com>
2023-02-22 23:15:51 +0100

    lol

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@ void print_alphabet_x10(void)
 
 	while  (i >= 9)
 	{
-	char letter;
+	char letters;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
+	for (letters = 'a'; letters <= 'z'; letters++)
+		_putchar('%c',letters);
 
 	_putchar('\n');
 	}
