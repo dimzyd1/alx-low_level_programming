1379cf1578d01f957a53891d20979218e48c2051
dimzyd1 <106175940+dimzyd1@users.noreply.github.com>
2023-02-27 16:57:38 +0100

    lol

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+*_strlen-returns the length of a string
+*@s:string that is passed into the function
+*Return:returns the variable length
+*/
+
+int _strlen(char *s)
+{
+	int length = strlen(*s);
+
+	return (length);
+}
