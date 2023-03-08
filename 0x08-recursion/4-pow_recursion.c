ecf458a6beba71630ddd5ca494d930c20ce50661
dimzyd1 <106175940+dimzyd1@users.noreply.github.com>
2023-03-08 21:13:46 +0100

    lol

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -8,12 +8,12 @@
  */
 int _pow_recursion(int x, int y)
 {
+	int result;
+
 	if (y < 0)
 		return (-1);
 	if (y == 0)
 		return (1);
-	int result;
-
 	result = x * _pow_recursion(x, y - 1);
 	return (result);
 }
